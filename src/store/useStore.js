9bd8acaeb4bc8046a108086d8e738fc3dd2fa4ed
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-16 05:02:15 +0100

    commit

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,16 +1,17 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import { v4 } from "uuid";
 
 const useNoteStore = create(
     persist(
         (set) => ({
-            notes: [{id:1, title: "London 2025", body: " Planning for my London travel next year"},
-                {id:2, title: "York St Johh", body: " Welcome to school"},
-                {id:3, title: "Digital Marketing", body: " Learn about photography in the first semester"},
-                {id:4, title: "YouTuber", body: " New Camera FX30 with sigma 16 mm lens"}
+            notes: [{id: v4() , title: "London 2025", body: " Planning for my London travel next year"},
+                {id: v4(), title: "York St Johh", body: " Welcome to school"},
+                {id: v4(), title: "Digital Marketing", body: " Learn about photography in the first semester"},
+                {id: v4(), title: "YouTuber", body: " New Camera FX30 with sigma 16 mm lens"}
             ],
-            addNotes: (addNote) => set((state) => ({ notes: [...state.notes,
-                {id:state.notes.length+1, title: addNote.title, body: addNote.body}]
+            addNotes: (addNote) => set((state) => ({ notes: [...state.notes, 
+                {id: v4(), title: addNote.title, body: addNote.body}]
             })),
         })
     )
