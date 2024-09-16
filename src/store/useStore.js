1cfc2507119c30fcbc6b1d1550f65859f5998218
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-16 04:48:23 +0100

    commit

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -7,9 +7,17 @@ const useNoteStore = create(
             notes: [{id:1, title: "London 2025", body: " Planning for my London travel next year"},
                 {id:2, title: "York St Johh", body: " Welcome to school"},
                 {id:3, title: "Digital Marketing", body: " Learn about photography in the first semester"},
-                {id:4, title: "YouTuber", body: " New Camera FX30 with sigma 16 mm lens"}],
+                {id:4, title: "YouTuber", body: " New Camera FX30 with sigma 16 mm lens"}
+            ],
+            addNotes: (addNote) => set((state) => ({ notes: [...state.notes,
+                {id:state.notes.length+1, title: addNote.title, body: addNote.body}]
+            })),
         })
     )
 )
 
 export default useNoteStore;
+
+//addNote: (note) => set((state) => ({ notes: [...state.notes,
+//{id:state.notes.length+1, title: note.title, body: note.body}]
+//})),
